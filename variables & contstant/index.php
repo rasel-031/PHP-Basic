6c494f3b74a1aefdb3a73aeec7c9484acdb3be61
rasel-031 <raselbishwas997@gmail.com>
2022-05-15 20:16:47 +0600

    learned varible, constant and show output using echo and print

---
+++ variables & contstant/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+echo 'Alhamdulillah'."\n";
+print 'Allah'."\n";
+
+//variable
+$name = "Rasel Bishwas";
+
+//double quotes means varibale treat as variable
+echo "My name is $name \n";
+
+//single quotes means all treat as string including varibales
+echo 'My name is $name'."\n";
+
+//constant
+define("NAME", "Bangladesh\n");
+echo NAME;
+
+$value = print("Allah \n");
+echo $value;
+?>
